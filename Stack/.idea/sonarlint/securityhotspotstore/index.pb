6bb2f1fe8c461794f022dd4f21ca5e026100c2f9
hackingBomb <int_kimty@naver.com>
2024-06-28 21:34:55 +0900

    add Stack

---
+++ Stack/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+=
+src/Main.java,9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138
+>
+src/Stack.java,8/d/8d3530559dec445825d0cf19f0dada6d7bc2655a
+9
+	Stack.iml,7/3/73edc7545990e06d24f3089bc22a63fca6cac9f7
+>
+src/stack.java,e/8/e872e470e2ccfd9ae69446a65a4247153578ae5c
+K
+src/Recursive/recusive.java,5/3/535fb6928d3cee740dea91af08f02126b3659427
---
+++ Stack/src/stack/stack.java
@@ -0,0 +1,50 @@
+public class stack {
+    private int top = -1;
+    private Object[] stack;
+
+    public stack(int size){
+        stack = new Object[size];
+    }
+
+    public boolean isEmpty(){
+        return top == -1;
+    }
+
+    public boolean isFull(){
+        return top == stack.length - 1;
+    }
+
+    public void push(Object value){
+        if(isFull()){
+            throw new RuntimeException("Stack is Full");
+        }
+        stack[++top] = value;
+    }
+
+    public Object pop(){
+        if(isEmpty()){
+            throw new RuntimeException("Stack is empty");
+        }
+        return stack[top--];
+    }
+
+    public Object peek(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+        } else if(isFull()){
+            System.out.println("Stack is Full");
+        }
+        return stack[top];
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Bottom | ");
+        for(int i = 0; i < top; i++){
+            sb.append(stack[i].toString() + " | ");
+        }
+        sb.append("Top");
+        return sb.toString();
+    }
+}
